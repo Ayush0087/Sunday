fac9d9620afd2d83ad91a9c0f0c9d4a6ccd2c538
Ayush0087 <54570255+Ayush0087@users.noreply.github.com>
2019-09-22 21:41:41 +0530

    till sunday

---
+++ SalesPerson/SalesPerson.Entities/Entities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreatOutdoor.Entities
+{
+    // Sales Person Interface
+    public interface ISalesPerson
+    {
+        string SalesPersonName { get; set; }
+        string SalesPersonEmail { get; set; }
+        string SalesPersonMobile { get; set; }
+        int SalesPersonID { get; set; }
+        string SalesPersonPassword { get; set; }
+
+    }
+
+    //Sales Person Class
+    public class SalesPerson : ISalesPerson
+    {
+
+        public string SalesPersonName { get; set; }
+        public string SalesPersonEmail { get; set; }
+        public string SalesPersonMobile { get; set; }
+        public int SalesPersonID { get; set; }
+        public string SalesPersonPassword { get; set; }
+
+    }
+}
